eda18b1a78550044fb4b9e171f47eeed10101721
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-05-31 13:31:53 +0900

    FIX : CORS설정, 채팅서버,배포 설정 Secret.yml

---
+++ src/main/java/org/duckdns/bidbuy/global/auth/controller/OAuthController.java
@@ -0,0 +1,12 @@
+package org.duckdns.bidbuy.global.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OAuthController {
+
+
+
+}
