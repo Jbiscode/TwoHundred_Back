ea3a80ea01ae3aed2b2873ae978d81e0066f36bc
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-05-31 23:16:34 +0900

    HOTFIX : Naver 엔드포인트 로직 수정

--- src/main/java/org/duckdns/bidbuy/global/auth/controller/OAuthController.java
+++ src/main/java/org/duckdns/bidbuy/global/auth/controller/OAuthController.java
@@ -1,7 +1,10 @@
 package org.duckdns.bidbuy.global.auth.controller;
 
+import java.math.BigInteger;
 import java.net.URI;
+import java.security.SecureRandom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/oauth2")
 public class OAuthController {
+  @Value("${oauth.naver.client-id}")
+  private String naverClientId;
+  @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
+  private String naverRedirectUri;
 
   @GetMapping("/redirect/naver")
   public ResponseEntity<Void> redirectNaver() {
+    String clientId = naverClientId;
+    String redirectURI = naverRedirectUri;
+    SecureRandom random = new SecureRandom();
+    String state = new BigInteger(130, random).toString(32);
+
+    String naverAuthUrl = String.format(
+            "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=%s",
+            clientId, redirectURI, state
+    );
+
     HttpHeaders headers = new HttpHeaders();
-    headers.setLocation(URI.create("https://api-bidbuy.duckdns.org/oauth2/authorization/naver"));
+    headers.setLocation(URI.create(naverAuthUrl));
     return new ResponseEntity<>(headers, HttpStatus.FOUND);
   }
 
