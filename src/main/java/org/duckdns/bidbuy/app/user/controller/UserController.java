f71ab25ddfd70af4b41b4a5578e3b9ed14bd4f03
dongsu <tlsehd930@gmail.com>
2024-06-03 14:08:10 +0900

    임시

--- src/main/java/org/duckdns/bidbuy/app/user/controller/UserController.java
+++ src/main/java/org/duckdns/bidbuy/app/user/controller/UserController.java
@@ -1,8 +1,12 @@
 package org.duckdns.bidbuy.app.user.controller;
 
 
+import org.duckdns.bidbuy.app.user.domain.User;
 import org.duckdns.bidbuy.app.user.service.UserService;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +19,10 @@ public class UserController {
 
   private final UserService userService;
 
+  @GetMapping(value = "/my")
+  public User getUser(Authentication authentication) {
+      Long userId = Long.valueOf(authentication.getName());
+
+  }
 
 }
