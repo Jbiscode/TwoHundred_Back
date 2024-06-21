b817fccee9ffcccc5514e3cb8debdf70f3d6f4d8
maeng <soon5796735@naver.com>
2024-06-21 17:55:02 +0900

    ✨Feat: 내 주변 거래 게시글 출력 기능 구현  - UserResponse DTO 따로 만들어서 주소값 받아와서 조건절로 적용헤서 addr1, addr2d와 일치하는 게시글 출력

--- src/main/java/org/duckdns/bidbuy/app/search/service/SearchService.java
+++ src/main/java/org/duckdns/bidbuy/app/search/service/SearchService.java
@@ -3,6 +3,7 @@ package org.duckdns.bidbuy.app.search.service;
 import lombok.RequiredArgsConstructor;
 import org.duckdns.bidbuy.app.article.domain.*;
 import org.duckdns.bidbuy.app.article.dto.ArticleResponse;
+import org.duckdns.bidbuy.app.search.dto.UserResponse;
 import org.duckdns.bidbuy.app.search.repository.SearchRepository;
 import org.duckdns.bidbuy.app.user.domain.User;
 import org.springframework.stereotype.Service;
@@ -25,11 +26,19 @@ public class SearchService {
         return searchRepository.totalCount(category, tradeMethod, tradeStatus, content);
     }
 
-    public List<User> findUser(Long id) {
-        return searchRepository.findUser(id);
-    }
-
     public List<LikeArticle> findLikeArticles(Category category, TradeMethod tradeMethod, String content) {
         return searchRepository.findLikeArticles(category, tradeMethod, content);
+    }
+
+    public List<User> findUserAddress(Long id) {
+        return searchRepository.findUserAddress(id);
+    }
+
+    public List<Article> searchLocation(Category category, TradeMethod tradeMethod, TradeStatus tradeStatus, String orderBy, int page, int size, List<UserResponse> address) {
+        return searchRepository.searchLocation(category, tradeMethod, tradeStatus, orderBy, page, size, address);
+    }
+
+    public Long myLocationtotalCount(Category category, TradeMethod tradeMethod, TradeStatus tradeStatus, List<UserResponse> address) {
+        return searchRepository.myLocationtotalCount(category, tradeMethod, tradeStatus, address);
     }
 }
