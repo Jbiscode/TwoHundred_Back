fb6f100c754fe63f5841c0bd2974e0d9e59b7b5f
dongsu <tlsehd930@gmail.com>
2024-06-25 16:58:32 +0900

    FEAT : 회원정보수정 API 업데이트

--- src/main/java/org/duckdns/bidbuy/app/user/controller/UserController.java
+++ src/main/java/org/duckdns/bidbuy/app/user/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.duckdns.bidbuy.app.article.domain.TradeStatus;
 import org.duckdns.bidbuy.app.review.dto.ReviewResponse;
 import org.duckdns.bidbuy.app.review.service.ReviewService;
+import org.duckdns.bidbuy.app.user.dto.MyInfoResponse;
 import org.duckdns.bidbuy.app.user.dto.MyProfileResponse;
 import org.duckdns.bidbuy.app.user.dto.MySalesResponse;
 import org.duckdns.bidbuy.app.user.dto.PageResponseDTO;
@@ -17,11 +18,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -33,11 +37,30 @@ public class UserController {
     private final UserService userService;
     private final ReviewService reviewService;
 
-    @Operation(summary = "내 정보 불러오기 API", description = "로그인된 사용자만 내 정보 불러올 수 있음")
+    @Operation(summary = "내 프로필 불러오기 API", description = "로그인된 사용자만 내 정보 불러올 수 있음")
     @GetMapping(value = "/me")
     public ResponseEntity<ApiResponse<?>> getMe() {
         MyProfileResponse responseDTO = userService.getMyProfile();
         ApiResponse<MyProfileResponse> response = new ApiResponse<>("200", "마이페이지 정보를 불러오는데 성공하였습니다.", responseDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "내 정보 불러오기 API")
+    @GetMapping(value = "/me/info")
+    public ResponseEntity<ApiResponse<?>> getMeInfo() {
+        MyInfoResponse responseDTO = userService.getMyInfo();
+        ApiResponse<MyInfoResponse> response = new ApiResponse<>("200", "내 정보를 불러오는데 성공했습니다.", responseDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "내 정보 수정하기 API")
+    @PutMapping(value = "/me/info",consumes = { MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<ApiResponse<?>> updateMeInfo(
+            @RequestPart(value = "myInfoResponseDTO") MyInfoResponse myInfoResponseDTO,
+            @RequestPart(value = "img", required = false) List<MultipartFile> list
+    ) throws IOException {
+        String result = userService.updateUserInfo(myInfoResponseDTO, list);
+        ApiResponse<MyInfoResponse> response = new ApiResponse<>("200", result, null);
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/org/duckdns/bidbuy/app/user/dto/MyInfoResponse.java
+++ src/main/java/org/duckdns/bidbuy/app/user/dto/MyInfoResponse.java
@@ -1,27 +1,17 @@
 package org.duckdns.bidbuy.app.user.dto;
 
 import lombok.*;
-import org.duckdns.bidbuy.app.user.domain.UserRole;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
+public class MyInfoResponse {
     private Long id;
+    private String username;
     private String password;
     private String email;
-    private String username;
-    private String name;
     private String addr1;
     private String addr2;
-    private String provider;
-    private String providerId;
     private String profileImageUrl;
-    private Integer score;
-    private Integer offerLevel;
-    private UserRole role;
-
-
-
 }
